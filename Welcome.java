6ed16ecc6109866ec8490f19c17ae1ddbd2d758e
HashiniSR <IT24610804@my.slii.lk>
2025-08-03 09:39:41 +0530

    LAB 01 Activity 01 Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Welcome
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your First Name:");
+		String firstName = input.nextLine();
+
+		System.out.print("Enter your Last Name:");
+		String lastName = input.nextLine();	
+
+		System.out.print("Welcome to the Second Year "+ firstName + " " + lastName);
+			
+	}
+
+}
